ae57b5ee1498f9b8979763b6d8bb899115e01a24
FerrisChan <ferris.chan@foxmail.com>
2018-08-15 00:53:16 +0800

    Site updated: 2018-08-15 00:53:16

---
+++ 2017/12/17/CSAPP/chap2/chap2/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// void hello(void) { printf("Hello, World!\n"); }
+
+int main() {
+  long a = 0;
+  if (0 == a) {
+    printf("it is l1\n");
+  } else {
+    printf("helloworld\n");
+  }
+  return 0;
+}
